481c2a31c74ba025a7438a823f7bdce14af3155b
Lucas <Lucas_201130@hotmail.com>
2022-10-29 16:55:02 -0300

    estudando solução para hanoi

---
+++ Cap_5/torreHanoi.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void torre(int, int, int, int);
+
+int main()
+{
+    //estudando solução
+}
+
+void torre(int base, int inicial, int fim, int temporario)
+{   
+            
+}
