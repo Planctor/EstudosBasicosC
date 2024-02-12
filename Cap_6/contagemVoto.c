7a085c5e39da4f4529d77ad694124ccaebc5c46c
Lucas Fernandes <47428909+Planctor@users.noreply.github.com>
2024-02-11 21:15:04 -0300

    Update contagemVoto.c
    correção

--- Cap_6/contagemVoto.c
+++ Cap_6/contagemVoto.c
@@ -4,7 +4,7 @@
 
 int main()
 {
-    //obs: a linguagem C não verifica o limete do array, tendo isso em mente pode se evitar problemas futuros
+    //obs: a linguagem C não verifica o limite do array, tendo isso em mente ajuda a evitar problemas
     int respostas[TAMANHO_RESPOSTAS] =  {1,2,6,4,8,5,9,7,8,10,1,6,3,8,6,10,3,8,2,7,6,5,7,6,5,7,6,8,6,7,5,6,6,5,6,7,5,6,4,8};
     int resultado[POSSIVEIS_RESPOSTAS] = {0}; //é preciso começar declarando pelo menos o primeiro valor da variavel ja que ela não se auto declara
 
